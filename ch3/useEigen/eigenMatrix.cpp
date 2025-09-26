10fb09d1aaf8b7e6d550a4c16d18762f510b4c82
SlaveFact0ry <guppy0118@gmail.com>
2025-09-26 13:49:57 +0900

    Eigen

--- ch3/useEigen/eigenMatrix.cpp
+++ ch3/useEigen/eigenMatrix.cpp
@@ -13,7 +13,7 @@ using namespace Eigen;
 int main(int argc, char **argv){
 	Matrix<float, 2, 3> matrix_23;
 
-	Vector3d v_ed;
+	Vector3d v_3d;
 	
 	Matrix<float, 3, 1> vd_3d;
 	
@@ -33,15 +33,13 @@ int main(int argc, char **argv){
 	}
 
 	v_3d << 3,2,1;
-	vd_3d<< 4,5,6;
+	v_3d<< 4,5,6;
 
-	Matrix<double, 2, 1> result = matrix_23.cat<double>() * v_3d;
+	Matrix<double, 2, 1> result = matrix_23.cast<double>() * v_3d;
 	cout << "[1,2,3;4,5,6]*[3,2,1]=" << result.transpose() <<endl;
 
 	Matrix<float, 2, 1> result2 = matrix_23 * vd_3d;
 	cout << "[1,2,3;4,5,6]*[4,5,6]: " << result2.transpose() <<endl;
-
-	cast<double>() * v_3d;
 
 	matrix_33 = Matrix3d::Random();
 	cout << "random matrix: \n" << matrix_33 << endl;
@@ -49,12 +47,12 @@ int main(int argc, char **argv){
 	cout << "sum: " << matrix_33.sum() << endl;
 	cout << "trace:" << matrix_33.trace() << endl;
 	cout << "times 10: \n " << 10* matrix_33 << endl;
-	cout << "inverse: \n" MM matrix_33.inverse() << endl;
+	cout << "inverse: \n" << matrix_33.inverse() << endl;
 	cout << "det:" << matrix_33.determinant() << endl;
 
-	SelfAdjointEigenSolver<Matrix3d> eigen_solver(matrix_33.transpose() * matrix_33)
+	SelfAdjointEigenSolver<Matrix3d> eigen_solver(matrix_33.transpose() * matrix_33);
 	cout << "Eigen values= \n" << eigen_solver.eigenvalues() <<endl;
-	cout << "Eigen vectors = \n" MM eigen_solver.eigenvectors() << endl;
+	cout << "Eigen vectors = \n" << eigen_solver.eigenvectors() << endl;
 
 	Matrix<double, MATRIX_SIZE, MATRIX_SIZE> matrix_NN
 		= MatrixXd::Random(MATRIX_SIZE, MATRIX_SIZE);
