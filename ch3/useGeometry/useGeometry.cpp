4081881985f298f4d9a1c9fb2e96fe14c95185b9
SlaveFact0ry <guppy0118@gmail.com>
2025-09-29 00:28:09 +0900

    Eigen - geometry

--- ch3/useGeometry/useGeometry.cpp
+++ ch3/useGeometry/useGeometry.cpp
@@ -18,7 +18,7 @@ int main(int argc, char **argv) {
 
 	Vector3d v(1,0,0);
 	Vector3d v_rotated = rotation_vector * v;
-	cout << " (1,0,0) agter rotation (by angle axis) = " v_rotated.transpose() << endl;
+	cout << " (1,0,0) agter rotation (by angle axis) = "<< v_rotated.transpose() << endl;
 	
 	v_rotated = rotation_matrix * v;
 	cout << "(1,0,0) after rotation (by matrix) ="<< v_rotated.transpose() << endl;
