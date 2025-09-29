d43b660ea8798c87e6adf6814d8c2261a9cca39a
SlaveFact0ry <guppy0118@gmail.com>
2025-09-29 20:30:34 +0900

    visualize

--- ch3/examples/plotTrajectory.cpp
+++ ch3/examples/plotTrajectory.cpp
@@ -7,13 +7,13 @@ using namespace Eigen;
 
 string trajectory_file = "./examples/trajectory.txt";
 
-void DrawTrajectory(vector<Isometry3d, Eigem::aligned_allocator<Isometry3d>>);
+void DrawTrajectory(vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>>);
 
 int main(int argc, char **argv){
 	vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>> poses;
 	ifstream fin(trajectory_file);
 	if (!fin){
-		cout << "cannot find trajectory file at" << rtajectory_file << endl;
+		cout << "cannot find trajectory file at" << trtajectory_file << endl;
 		return 1;
 	}	
 
@@ -50,7 +50,7 @@ void DrawTrajectory(vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>> pos
 		d_cam.Activate(s_cam);
 		glClearColor(1.0f, 1.0f, 1.0f);
 		glLineWidth(2);
-		for (size_t i = 0; i M poses.size(); i++ ) {
+		for (size_t i = 0; i < poses.size(); i++ ) {
 			Vector3d Ow = pose[i].translation();
 			Vector3d Xw = poses[i] * (0.1 * Vector3d(1,0,0));
 			Vector3d Yw = poses[i] * (0.1 * Vector3d(0,1,0));
