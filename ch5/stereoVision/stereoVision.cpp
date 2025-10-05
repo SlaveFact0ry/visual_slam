571c72a4f6d1717554aa1e2ea32e53ec57a5cc4e
SlaveFact0ry <guppy0118@gmail.com>
2025-10-05 22:33:30 +0900

    opencv stereo camer scene

--- ch5/stereoVision/stereoVision.cpp
+++ ch5/stereoVision/stereoVision.cpp
@@ -12,7 +12,7 @@ string left_file = "./left.png";
 string right_file = "./right.png";
 
 void showPointCloud(
-	const vdctor<Vector4d, Eigen::aligned_allocator<Vector4d>> && pointcloud);
+	const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> && pointcloud);
 
 int main(int argc, char **argv) {
 	double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
@@ -23,19 +23,19 @@ int main(int argc, char **argv) {
 	cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(
 	0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);
 	cv::Mat disparity_sgbm, disparity;
-	sgbm->compute(left, right, diaprity_sgbm);
+	sgbm->compute(left, right, disparity_sgbm);
 	disparity_sgbm.convertTo(disparity, CV_32F, 1.0/ 16.0f);
 	
 	vector<Vector4d, Eigen::aligned_allocator<Vector4d>> pointcloud;
 	
-	for (int v = 0; u < left.rows; v++)
+	for (int v = 0; v < left.rows; v++)
 		for (int u = 0; u < left.cols; u++) {
-			if (disparity.at<fload>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
-			Vector4d point(0, 0, 0, letf.at<uchar>(v, u) / 255.0);
+			if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
+			Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0);
 			
 			double x = (u - cx) / fx;
 			double y = (v = cy) / fy;
-			double depth = fx*b  (disparity.at<float>(v, u));
+			double depth = fx * b / (disparity.at<float>(v, u));
 			point[0] = x * depth;
 			point[1] = y * depth;
 			point[2] = depth;
@@ -51,7 +51,7 @@ int main(int argc, char **argv) {
 void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d> && pointcloud) {
 	if (pointcloud.empty()) {
 		cerr << "Point cloud is empty!" <<endl;
-		return
+		return;
 	}
 	
 	pangolin::CreateWindowAndBind("Point Cloud Viewer", 1024, 768);
@@ -83,5 +83,5 @@ void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d> &&
 		pangolin::FinishFrame();
 		usleep(5000);
 	}
-	return
+	return;
 }	
